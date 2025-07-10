9cc91e613335d0310ac79d9615ef8ebd72a7e358
Ankit Rishav <ankitrishavkr@gmail.com>
2025-07-10 09:52:55 +0000

    make everything a bit small and I cant see the footer now !!!! why

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -115,9 +115,9 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <div className="flex flex-col min-h-screen">
-            <div className="flex-1">
+            <main className="flex-1">
               {children}
-            </div>
+            </main>
             <Footer />
           </div>
           <Toaster />
